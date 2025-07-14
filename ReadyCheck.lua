1bd09b7e608ef409cad7da013ab991696ef6d107
August <87087046+augustlh@users.noreply.github.com>
2025-07-14 22:09:58 +0200

    Added guildgroup and "alive" check

--- ReadyCheck.lua
+++ ReadyCheck.lua
@@ -4,19 +4,18 @@ function InCombat()
     return (UnitAffectingCombat("player") or InCombatLockdown())
 end
 
--- Not sure what to name this function
--- Returns true if the player is NOT in combat, is in a raid and is either assist or lead, else false
 function IsValidScenario()
     return not InCombat()
         and IsInRaid()
         and (UnitIsGroupLeader("player") or UnitIsGroupAssistant("player"))
+        and IsInGuildGroup()
 end
 
 function GBI:ReadyCheck()
     if IsValidScenario() then
         print("--- READY CHECK TIMER STARTED! ---")
         C_Timer.After(5, function()
-            if IsValidScenario() then
+            if IsValidScenario() and not UnitIsDeadOrGhost("player") then
                 DoReadyCheck()
             end
         end)
